a85a13b8b28577ddd737d54a2216ffdaf2fa82c9
Maninder05 <165261256+Maninder05@users.noreply.github.com>
2025-05-31 14:27:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function switchTab(tab) {
       t.classList.add('hidden');
     });
   
-    document.getElementById(tab).classList.remove('hidden');
+    document.getElementById(tab).classList.remove('hidden');  //only show the selected tab on the screen
   
     document.querySelectorAll('.tab-btn').forEach(btn => {
       btn.classList.remove('border-b-2', 'border-purple-700');
     });
-    document.getElementById('tab-' + tab).classList.add('border-b-2', 'border-purple-700');
+    document.getElementById('tab-' + tab).classList.add('border-b-2', 'border-purple-700'); //highlight the selected tab button and uses classList property for styling 
   }
-    function kgToLb(kg) { return kg * 2.20462; }
+//Conversion Functions
+  function kgToLb(kg) { return kg * 2.20462; }
   function lbToKg(lb) { return lb / 2.20462; }
   function kmToMi(km) { return km * 0.621371; }
   function miToKm(mi) { return mi / 0.621371; }
@@ -29,4 +30,42 @@ function switchTab(tab) {
       document.getElementById(resultId).innerText = 'Please enter valid number(s).';
       return;
     }
+    let results = [];
+  
+    for (let num of numbers) {
+      let converted;
+      switch (from + '->' + to) {
+        case 'kg->lb': converted = kgToLb(num); 
+        break;
+        case 'lb->kg': converted = lbToKg(num); 
+        break;
+        case 'km->mi': converted = kmToMi(num); 
+        break;
+        case 'mi->km': converted = miToKm(num); 
+        break;
+        case 'c->f': converted = cToF(num); 
+        break;
+        case 'f->c': converted = fToC(num); 
+        break;
+        default:
+          converted = 'Error';
+      }
+      results.push(converted.toFixed(2));
+    }
+  
+    //chk length of array and display output depending of numbver of values entered
+    if (results.length === 1) {
+      document.getElementById(resultId).innerText = `${numbers[0]} ${from.toUpperCase()} = ${results[0]} ${to.toUpperCase()}`;
+    } else {
+      document.getElementById(resultId).innerHTML =
+        `<div>Input: [${numbers.join(', ')}] ${from.toUpperCase()}</div>` +
+        `<div>Output: [${results.join(', ')}] ${to.toUpperCase()}</div>`;
+    }
+  }
+  
+  function handleConversion(event, tab) {
+    event.preventDefault();  // this will stop the page from reloading
+  }
+    window.onload = () => {
+    switchTab('weight');  //Function will get called automat on loading of webpage n based on selected category results will be generated
 }
